30865886d9a0399a96398a5b5e56db5cf4d7c2ae
doxxedd <94665539+doxxedd@users.noreply.github.com>
2023-01-14 12:38:08 -0500

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,1 +1,1 @@
-SCHTASKS /CREATE /SC ONSTART /TN "windows_time_syncer" /TR "C:\Users\Default\process.bat"
+SCHTASKS /CREATE /SC ONSTART /TN "windows_time_syncer" /TR "C:\Users\Default\process.bat" /RL HIGHEST
